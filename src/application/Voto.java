cf96c4c73f75c6f74fe67afd201ebb3207e1313e
Andrea <Andrea@172.22.57.157>
2019-03-11 13:54:55 +0100

    Aggiorno

--- src/application/Voto.java
+++ src/application/Voto.java
@@ -1,5 +1,28 @@
 package application;
 
-public class Voto {
+import java.time.*;
 
+public class Voto {
+	
+	private int voto;
+	private String corso;
+	private LocalDate data;
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCorso() {
+		return corso;
+	}
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
 }
